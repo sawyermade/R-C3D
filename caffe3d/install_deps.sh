5b5abaa77869df31dbdaecfd70df75c860680f17
sawyermade <sawyermade0@gmail.com>
2019-01-26 20:42:05 -0500

    added shit

---
+++ caffe3d/install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install build-essential protobuf-compiler libhdf5-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libopenblas-dev libleveldb-dev
+export CPATH=/usr/include/hdf5/serial/
+
