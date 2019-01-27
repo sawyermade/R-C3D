42f6ba94c87e49b132f221c0a656096d4e5494c0
Daniel Sawyer <sawyermade0@gmail.com>
2019-01-27 14:04:26 -0500

    fixed deps

--- caffe3d/install_deps.sh
+++ caffe3d/install_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install build-essential protobuf-compiler libhdf5-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libopenblas-dev libleveldb-dev libsnappy-dev
+sudo apt install build-essential protobuf-compiler libhdf5-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libopenblas-dev libleveldb-dev libsnappy-dev youtube-dl
 export CPATH=/usr/include/hdf5/serial/
 
